45321075d531c5d63e1eed2d17ea208a758d0f4d
Pooja Gurnule <poojagurnule2411999@gmail.com>
2023-04-22 16:01:43 +0530

    Assignment Done

---
+++ src/main/java/com/example/UrlHitCounter/UrlHitCounterApplication.java
@@ -0,0 +1,14 @@
+package com.example.UrlHitCounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlHitCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlHitCounterApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/example/UrlHitCounter/services/UrlHitService.java
@@ -0,0 +1,30 @@
+package com.example.UrlHitCounter.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.validation.ObjectError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UrlHitService {
+
+    private static int hitCount = 0;
+
+    public int getHitCount() {
+        return hitCount++;
+    }
+
+    public Map<String, Object> getHitCount(String username){
+        getHitCount();
+        Map<String, Object> response = new HashMap<>();
+        response.put("username", username);
+        response.put("hitcount", hitCount);
+        return response;
+    }
+}
+
+
+
+
+
